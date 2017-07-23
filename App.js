684b2170af88c1685b08254ed5f37248847004a7
James Le <le_j6@denison.edu>
2017-07-22 20:36:34 -0700

    Main App File

--- App.js
+++ App.js
@@ -1,23 +1,40 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import { Provider, connect } from 'react-redux';
+import { Font, AppLoading } from 'expo';
+import store from './app/redux/store';
+import Router from './app/config/routes';
 
-export default class App extends React.Component {
+export default class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      isReady: false,
+    }
+  }
+
+  async componentWillMount() {
+    await Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+      'Pacifico': require('./app/assets/fonts/Pacifico.ttf'),
+      'Ionicons': require('native-base/Fonts/Ionicons.ttf'),
+    });
+
+    this.setState({isReady: true});
+  }
+
+
   render() {
+    if (!this.state.isReady) {
+      return <AppLoading />;
+    }
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <Provider store={store}>
+        <Router />
+      </Provider>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+AppRegistry.registerComponent('main', () => App);
